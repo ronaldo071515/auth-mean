91a68a9089aa3d0a6d69a991c51e43c67208099e
Ronaldo <rtorres66@misena.edu.co>
2023-05-08 22:30:46 -0500

    config enviroments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+
+
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017'),
+    ConfigModule.forRoot(),
+
+    MongooseModule.forRoot( process.env.MONGO_URI ),
     AuthModule,
   ],
   controllers: [],
