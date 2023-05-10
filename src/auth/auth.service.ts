6c3590efa479c0dae669fdb17452c80efd8027f0
Ronaldo <rtorres66@misena.edu.co>
2023-05-10 17:06:25 -0500

    schema user implement and create User end

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,42 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
+import { Injectable, BadRequestException, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import * as bcryptjs from 'bcryptjs';
+
+import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+
+  constructor(
+    /* User.name es el nombre de nuestro modelo */
+    @InjectModel(User.name) 
+    private userModel: Model<User>
+  ){}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    
+    try {
+      
+      const { password, ...userData } = createUserDto;
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync(password, 10),
+        ...userData
+      });
+      await newUser.save();
+      const { password:_, ...user } = newUser.toJSON() 
+      return user;
+      
+    } catch (error) {
+      if(error.code === 11000) {
+        throw new BadRequestException(`${ createUserDto.email } already exists!`);
+      }
+      throw new InternalServerErrorException('Somethig terrible happend!');
+    }
+
   }
 
   findAll() {
